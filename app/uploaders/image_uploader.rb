8cf1e30fa155bd1e6d0a21d9fa75e9134d65970c
BriSkii <bwhit034@outlook.com>
2016-09-04 21:33:18 -0400

    Setup Carrierwave, S3 and Figaro

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -41,6 +41,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def extension_white_list
   #   %w(jpg jpeg gif png)
   # end
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
